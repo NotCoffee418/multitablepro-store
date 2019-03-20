1d4ee888c683b6896492ed24c60964e9b2decca1
Nadromar <nadromar@github.com>
2019-03-20 01:58:05 +0100

    Favicons

--- application/views/shared/header.php
+++ application/views/shared/header.php
@@ -13,7 +13,24 @@
 		else echo $this->SiteSettings->get('site_title');
 	?></title>
 
-	<!-- <link rel="icon" href="/favicon.ico"> -->
+	<link rel="apple-touch-icon" sizes="57x57" href="/img/icons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/img/icons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/img/icons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/img/icons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/img/icons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/img/icons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/img/icons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/img/icons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/img/icons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/img/icons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png">
+	<link rel="manifest" href="/img/icons/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/img/icons/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<link href="/css/style.css" rel="stylesheet" type="text/css">
 </head>
